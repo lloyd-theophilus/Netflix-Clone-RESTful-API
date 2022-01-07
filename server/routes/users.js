614faf447895461dbec0d1f49887218793874dd7
Lloyd Theophilus <lloydtheophilus@gmail.com>
2022-01-07 14:03:25 +0000

    added a git ignore file

--- server/routes/users.js
+++ server/routes/users.js
@@ -68,7 +68,7 @@ router.get('/', verify, async(req, res) => {
 
         try {
             //If there is a query, find all users but display up 20,also if there is no query, then find all users
-           const users = query ? await User.find().sort({_id:-1}).limit(20) : await User.find()
+           const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find()
             res.status(200).json(users)
         } catch (error) {
             res.status(500).json(error)
@@ -85,21 +85,6 @@ router.get('/', verify, async(req, res) => {
 router.get('/stats', async(req, res) =>{
     const today = new Date()
     const lastYear = today.setFullYear(today.setFullYear() - 1)
-
-    const monthsArray = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
 
     try {
         const data = await User.aggregate([
